b99d54af4954cfd735cf71c940841357a16e9710
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-10-26 12:13:28 -0400

    Swapped the getAllRequests to return the database data

--- src/getAllRequests.js
+++ src/getAllRequests.js
@@ -2,7 +2,7 @@
 const DatabaseUtil = require("./utils/DatabaseUtil");
 
 exports.http = (request, response) => {
-    let database = new DatabaseUtil();
-
-    response.status(200).send('Get All Requests - Hello World!');
+    const database = new DatabaseUtil();
+    const data = database.GetAll();
+    response.status(200).send(JSON.stringify(data));
 };

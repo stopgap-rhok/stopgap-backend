e68dea21d769a993fb8c8a163e7916bcfac7c509
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-10-26 12:22:19 -0400

    Added Ramp Request

--- src/models/rampRequest.js
+++ src/models/rampRequest.js
@@ -1,5 +1,17 @@
 class RampRequest {
-    
+    businessName
+    address
+    isBusinessOwner
+    businessContact
+    requestorEmail
+
+    constructor({businessName, address, isBusinessOwner, businessContact, requestorEmail}) {
+        this.businessName = businessName;
+        this.address = address;
+        this.isBusinessOwner = isBusinessOwner;
+        this.businessContact = businessContact;
+        this.requestorEmail = requestorEmail;
+    }
 }
 
 export class RampRequest;
--- src/utils/DatabaseUtil.js
+++ src/utils/DatabaseUtil.js
@@ -1,30 +1,30 @@
 const RampRequest = require("..//models/RampRequest");
 
 class DatabaseUtil {
-    dbData = [];
+    dbData;
 
     constructor() {
-        this.dbData.push({
+        this.dbData.push(new RampRequest({
             businessName: "My Business",
             address: "123 Example Street"
             isBusinessOwner: false,
             businessContact: "",
             requestorEmail: "person@gmail.com"
-        });
-        this.dbData.push({
+        }));
+        this.dbData.push(new RampRequest({
             businessName: "Cabannas",
             address: "Cabana Way"
             isBusinessOwner: true,
-            businessContact: "example@business.com"
+            businessContact: "example@business.com",
             requestorEmail: "example@business.com"
-        });
-        this.dbData.push({
+        }));
+        this.dbData.push(new RampRequest({
             businessName: "Oni",
             address: "343 Mombassa St"
             isBusinessOwner: true,
-            businessContact: "613-552-1234"
+            businessContact: "613-552-1234",
             requestorEmail: "example@business.com"
-        });
+        }));
     }
 
     upload(data) {

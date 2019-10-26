5e95307ebe94d4f1e8e75175eb579a593473e2c1
macdja38 <jakeincanada@icloud.com>
2019-10-26 16:59:38 -0400

    Fix JSON.stringifying circular dependencies in dumpRequest

--- src/dumpRequest.js
+++ src/dumpRequest.js
@@ -1,12 +1,17 @@
 'use strict';
 
+const util = require('util');
+
+
 module.exports.dumpRequest = (request, response) => {
-    console.table(request);
-    console.table(response);
+    console.log(request);
+    console.log(response);
 
     const jsonReturn = {
-        requestJson: JSON.stringify(request),
-        responseJson: JSON.stringify(response)
+        requestJson: util.inspect(request, {showHidden: false, depth: 1}),
+        responseJson: util.inspect(response, {showHidden: false, depth: 1})
     };
+
+    console.log(jsonReturn);
     response.status(200).send(JSON.stringify(jsonReturn));
 };

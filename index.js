949713c3d3bf69115c4e916d01b8b4611a92e869
Maxime Boyer <maxime.boyer@me.com>
2019-10-26 15:33:15 -0400

    Serverless testing

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
 
-exports.http = (request, response) => {
-  response.status(200).send('Hello World!');
+require("./src/getAllRampRequests.js");
+require("./src/getFile.js");
+require("./src/getRampRequest.js");
+require("./src/uploadRampRequest.js");
+
+exports.getAllRampRequests = (request, response) => {
+  getAllRampRequests.getAllRampRequests(request, response);
+};
+
+exports.getFile = (request, response) => {
+  getFile.getFile(request, response);
+};
+
+exports.getRampRequest = (request, response) => {
+  getRampRequest.getRampRequest(request, response);
+};
+
+exports.uploadRampRequest = (request, response) => {
+  uploadRampRequest.uploadRampRequest(request, response);
 };

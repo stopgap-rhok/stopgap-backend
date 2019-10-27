8f447e83cd2ee0ec9579ac7aaffff3c687f4654d
macdja38 <jakeincanada@icloud.com>
2019-10-27 11:52:27 -0400

    Correct reference to db.

--- src/services/DatabaseService.js
+++ src/services/DatabaseService.js
@@ -67,7 +67,7 @@ class DatabaseService {
     }
 
     async deleteByRequestId(id) {
-        const deleteDocument = db.collection(this.requestCollectionName).doc(id);
+        const deleteDocument = this.firestore.collection(this.requestCollectionName).doc(id);
         let deleteDoc = deleteDocument.delete();
         return true;
     }

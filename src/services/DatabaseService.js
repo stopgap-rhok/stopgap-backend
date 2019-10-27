f3e130e0fb2adb60316bdf63dc4183330d620d01
Andrew Godfroy <killerrin@users.noreply.github.com>
2019-10-26 20:35:08 -0400

    Implemented basic Pagination

--- src/services/DatabaseService.js
+++ src/services/DatabaseService.js
@@ -1,5 +1,7 @@
 const RampRequest = require("../models/RampRequest");
-const {Firestore} = require('@google-cloud/firestore');
+const {
+    Firestore
+} = require('@google-cloud/firestore');
 var admin = require("firebase-admin");
 const uuidv4 = require('uuid/v4');
 
@@ -10,37 +12,45 @@ admin.initializeApp({
 class DatabaseService {
     constructor() {
         this.firestore = new Firestore();
-        
+
     }
 
     async create(data) {
         const id = uuidv4();
         data.requestId = id;
 
-        const document = this.firestore.doc('reports/'+data.requestId);
+        const document = this.firestore.doc('reports/' + data.requestId);
         await document.set(data);
         return id;
     }
     async getAll() {
         const document = admin.firestore().collection('reports');
         let allDoc = await document.get()
-        .then(snapshot=>{
-            const results = [];
-            snapshot.forEach(doc => {
-                results.push(doc.data());
+            .then(snapshot => {
+                const results = [];
+                snapshot.forEach(doc => {
+                    results.push(doc.data());
+                });
+                return results;
             });
-            return results;
-        });
         return allDoc;
     }
 
     async getAllPaginated(skip, take) {
-        const document = this.firestore.doc('reports');
-        return [];
+        const document = admin.firestore().collection('reports');
+        let allDoc = await document.get()
+            .then(snapshot => {
+                const results = [];
+                snapshot.skip(skip).take(take).forEach(doc => {
+                    results.push(doc.data());
+                });
+                return results;
+            });
+        return allDoc;
     }
 
-    async getByRequestId(id) { 
-        const document = this.firestore.doc('reports/'+id);
+    async getByRequestId(id) {
+        const document = this.firestore.doc('reports/' + id);
         let doc = await document.get();
         return doc;
     }
